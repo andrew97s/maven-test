0472a7dff7f3009d21d97ada35657342927b04a4
andrew <1445481863@qq.com>
2022-07-02 08:40:20 +0800

    Initial commit

---
+++ test1/src/main/java/test/Appplication.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Appplication {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO THERE THIS IS TEST1 APP !");
+    }
+}
---
+++ test2/src/main/java/test2/Application.java
@@ -0,0 +1,7 @@
+package test2;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("HELLO THERE THIS IS TEST2 APP !!");
+    }
+}
